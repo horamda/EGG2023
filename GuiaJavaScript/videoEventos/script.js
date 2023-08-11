494cc47743165758ba1762ffd09cd21274794eb5
Horacio Yeuchuk <horamda@gmail.com>
2023-08-11 20:16:58 -0300

    ejercicio video 1

---
+++ GuiaJavaScript/videoEventos/script.js
@@ -0,0 +1,12 @@
+function saludar(){
+    console.log("Bienvenido!!!");
+}
+
+function retar(tipo) {
+    console.log(tipo);
+    console.log("Epa! sali de arriba !!!!");
+}
+
+function meHicisteClick() {
+    console.log("Click 3");
+}
