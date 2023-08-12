ea4fb1d99b4d51c6d58d3bb6454cbf4d59914613
Horacio Yeuchuk <horamda@gmail.com>
2023-08-11 23:02:06 -0300

    2do commit

--- GuiaJavaScript/videoEventos/script.js
+++ GuiaJavaScript/videoEventos/script.js
@@ -8,5 +8,29 @@ function retar(tipo) {
 }
 
 function meHicisteClick() {
-    console.log("Click 3");
+    console.log("me hiciste Click ");
+}
+
+window.onload = saludar;
+
+document.getElementById("123").onmouseover = function(){
+    retar("soy un input");
+}
+document.getElementById("btn").onclick=meHicisteClick;
+
+let titulos = document.getElementsByName("titulo");
+let pes= document.getElementsByTagName("p");
+let input= document.getElementById("123");
+let boton= document.getElementById("btn");
+
+titulos.forEach((e)=>{
+e.style.color="red";
+})
+
+for (const e of pes){
+    e.innerHTML="EGG";
+}
+
+input.onkeydown=()=>{
+    if(input.value.length>10) input.value="Chiquito";
 }
